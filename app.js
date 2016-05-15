24a2ed00ad9b7cb2cb612b58556d21072831e847
Aayush Shah <aayush.shah15@gmail.com>
2016-05-15 04:17:38 +0000

    refactored code

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
 var seedDB = require("./seeds");
+var Comment = require("./models/comment");
 // requiring all the dependencies
 
 seedDB();
@@ -36,7 +37,7 @@ app.get("/campgrounds", function(req, res){
             if(err){
                 console.log(err);
             } else {
-                res.render("index", {campgrounds:allCampgrounds})
+                res.render("campgrounds/index", {campgrounds:allCampgrounds})
             }
         })
       // res.render("campgrounds", {campgrounds: campgrounds});
@@ -62,7 +63,7 @@ app.post("/campgrounds", function(req, res){
 });
 
 app.get("/campgrounds/new", function(req, res){
-   res.render("new.ejs"); 
+   res.render("campgrounds/new"); 
 });
 
 app.get("/campgrounds/:id", function(req, res) {
@@ -71,10 +72,42 @@ app.get("/campgrounds/:id", function(req, res) {
         if (err){
             console.log(err);
         } else {
-            res.render("show", {campground: foundCampground});
+            res.render("campgrounds/show", {campground: foundCampground});
         }
     });
-})
+});
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    // find campground by ID
+    
+    Campground.findById(req.params.id, function(err, campground){
+       if (err){
+           console.log(err);
+       } else {
+           res.render("comments/new", {campground: campground});
+       }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    //lookup campground by ID
+    Campground.findById(req.params.id, function(err, campground) {
+       if (err){
+           console.log(err);
+           res.redirect("/campgrounds");
+       } else {
+           Comment.create(req.body.comment, function(err, comment){
+               if (err){
+                   console.log(err);
+               } else {
+                   campground.comments.push(comment);
+                   campground.save();
+                   res.redirect("/campgrouds/"+campground._id);
+               }
+           });
+       }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The server is running");
